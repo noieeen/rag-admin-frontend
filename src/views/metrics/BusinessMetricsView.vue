da3865766875f9b1c212a7fe4442dc88f990e1fd
noieeen <tharnnn1401@gmail.com>
2025-10-06 20:51:18 +0700

    fixed error

--- src/views/metrics/BusinessMetricsView.vue
+++ src/views/metrics/BusinessMetricsView.vue
@@ -11,7 +11,7 @@
           <button
             class="rounded-md border border-border px-3 py-2 text-sm"
             @click="exportMetrics"
-            :disabled="!metrics.value"
+            :disabled="query.data.value.length === 0"
           >
             Export JSON
           </button>
